6a67e82f4a653e6d08033e3c87f8a54fe04dd03a
Dmitrijs Zubriks <dima.zubrik@gmail.com>
2013-11-27 11:37:12 +0000

    Docstring for PizzaTopping

--- src/io/dizlv/pizza/PizzaTopping.java
+++ src/io/dizlv/pizza/PizzaTopping.java
@@ -1,18 +1,36 @@
 package io.dizlv.pizza;
 
+/**
+ * Represents pizza's topping. Holds information about name.
+ * @author Dmitrijs Zubriks
+ */
 public class PizzaTopping extends PizzaIngredient {
 
     private String name;
 
+    /**
+     * Constructor. Initializes variables with values.
+     * @param cost ingredient cost.
+     * @param isVegetarian is it vegetarian?
+     * @param name ingredient name.
+     */
     public PizzaTopping(double cost, boolean isVegetarian, String name) {
         super(cost, isVegetarian);
         this.name = name;
     }
 
+    /**
+     * Accessor method to get ingredient name.
+     * @return ingredient name.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Mutator method to set ingredients new name.
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
     }
