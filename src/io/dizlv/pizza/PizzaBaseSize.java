ce61ce1942644a1d4eb89a66ac0c67ea20203075
Dmitrijs Zubriks <dima.zubrik@gmail.com>
2013-11-27 11:28:52 +0000

    Docstrings for PizzaBaseSize

--- src/io/dizlv/pizza/PizzaBaseSize.java
+++ src/io/dizlv/pizza/PizzaBaseSize.java
@@ -1,5 +1,9 @@
 package io.dizlv.pizza;
 
+/**
+ * Enumeration that represents pizza's base size. It can be thin or thick.
+ * @author Dmitrijs Zubriks
+ */
 public enum PizzaBaseSize {
     THIN, THICK
 }
