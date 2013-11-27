e98eac6f92e738ddb30d3c5603f4c84ab907f02e
Dmitrijs Zubriks <dima.zubrik@gmail.com>
2013-11-27 11:27:16 +0000

    Docstrings for PizzaBaseSize

--- src/io/dizlv/pizza/PizzaBase.java
+++ src/io/dizlv/pizza/PizzaBase.java
@@ -1,18 +1,36 @@
 package io.dizlv.pizza;
 
+/**
+ * PizzaIngredient child. Represents PizzaBase. Holds information about it's size.
+ * @author Dmitrijs Zubriks
+ */
 public class PizzaBase extends PizzaIngredient {
 
     private PizzaBaseSize pizzaBaseSize;
 
+    /**
+     * Constructor calls parents constructor and initializes base size variable.
+     * Since it is pizza's base, it's vegetarian.
+     * @param cost base cost.
+     * @param pizzaBaseSize base size.
+     */
     public PizzaBase(double cost, PizzaBaseSize pizzaBaseSize) {
         super(cost, true);
         this.pizzaBaseSize = pizzaBaseSize;
     }
 
+    /**
+     * Accessor method to get pizza's base size.
+     * @return pizza's base size.
+     */
     public PizzaBaseSize getPizzaBaseSize() {
         return pizzaBaseSize;
     }
 
+    /**
+     * Mutator method. Set new pizza's base size.
+     * @param pizzaBaseSize enumeration that represents pizza's base size.
+     */
     public void setPizzaBaseSize(PizzaBaseSize pizzaBaseSize) {
         this.pizzaBaseSize = pizzaBaseSize;
     }
